bbcc2c841872aeb487d18d16f27548b79cd46ba8
hotsuma kawashima <hkawashimajob1992@gmail.com>
2024-04-27 12:59:51 +0900

    simple web server in go

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	var httpServer http.Server
+	http.HandleFunc("/", handler)
+	log.Println("Server started at port 8080")
+	httpServer.Addr = ":8080"
+	log.Println(httpServer.ListenAndServe())
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World")
 }
